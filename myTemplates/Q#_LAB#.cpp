2a8fefd94c82e6159200adb2d426a844a8880cca
meechtheballer99 <meechtheballer99@users.noreply.github.com>
2025-07-27 19:26:17 -0400

    Add myTemplates directory

---
+++ myTemplates/Q#_LAB#.cpp
@@ -0,0 +1,48 @@
+// CIS-200-LAB ##INSERT LAB NUMBER HERE## -DemetriusJohnson.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+//Author: Demetrius E Johnson
+//Date: 00 MONTH 2021
+//Last Modification Date: 00-00-2021
+//Purpose: *****
+
+
+*/
+
+/*
+Question #:
+
+
+
+*/
+
+
+
+#include <iostream>
+#include<assert.h>
+using namespace std;
+
+
+//FUNCTION DECLARATIONS
+
+
+
+//FUNCTION DECLARATIONS
+
+
+
+int main()
+{
+
+
+
+    cout << endl << endl << "The program has finished execution....now exiting...thank you....\n\n";
+    system("pause");
+
+    return 0;
+}
+
+
+//FUNCTION DEFINITIONS BELOW THIS LINE
+
